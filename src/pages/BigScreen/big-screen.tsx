ef1d46c0fe7628affd64bff646f450632f57a093
cuijie <969423756@qq.com>
2022-10-19 16:28:19 +0800

    feat(init): init

---
+++ src/pages/BigScreen/big-screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './big-screen.css';
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page Bigscreen/big-screen</h1>
+    </div>
+  );
+}
